4196da4c66d43e84f4adc6905835a899ab099eda
Nguyễn Đức Duy Anh <duyanhfwofficial@gmail.com>
2024-06-11 09:08:21 +0700

    fix something

--- 13_other_practice/lesson1.py
+++ 13_other_practice/lesson1.py
@@ -1,5 +1,21 @@
-def UCLN(x):
-    pass
+# Nhập hai số từ terminal
+num1 = int(input("Nhập số thứ nhất: "))
+num2 = int(input("Nhập số thứ hai: "))
 
-def BCNN(x):
-    pass
+# Hàm để tính ước chung lớn nhất
+def calc_gcd(x, y):
+    while(y):
+        x, y = y, x % y
+    return x
+
+# Hàm để tính bội chung nhỏ nhất
+def calc_lcm(x, y):
+    lcm = (x*y)//calc_gcd(x,y)
+    return lcm
+
+# Tính ước chung lớn nhất và bội chung nhỏ nhất
+gcd = calc_gcd(num1, num2)
+lcm = calc_lcm(num1, num2)
+
+print("Ước chung lớn nhất của", num1, "và", num2, "là:", gcd)
+print("Bội chung nhỏ nhất của", num1, "và", num2, "là:", lcm)
