44496b2701edb4338ef4938fc1e5bd9951c8150b
Nguyễn Đức Duy Anh <duyanhfwofficial@gmail.com>
2024-06-11 09:08:53 +0700

    fix something

--- 13_other_practice/lesson1.py
+++ 13_other_practice/lesson1.py
@@ -1,19 +1,15 @@
-# Nhập hai số từ terminal
 num1 = int(input("Nhập số thứ nhất: "))
 num2 = int(input("Nhập số thứ hai: "))
 
-# Hàm để tính ước chung lớn nhất
 def calc_gcd(x, y):
     while(y):
         x, y = y, x % y
     return x
 
-# Hàm để tính bội chung nhỏ nhất
 def calc_lcm(x, y):
     lcm = (x*y)//calc_gcd(x,y)
     return lcm
 
-# Tính ước chung lớn nhất và bội chung nhỏ nhất
 gcd = calc_gcd(num1, num2)
 lcm = calc_lcm(num1, num2)
 
